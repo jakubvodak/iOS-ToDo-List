0ed574f4004a9bad6c377157fd2fd35d044a2459
Jakub Vodak <jakubvodak@Jakub-MacBook-Air.local>
2021-12-02 19:11:18 +0100

    Initial commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Jakub Vodak on 02.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
